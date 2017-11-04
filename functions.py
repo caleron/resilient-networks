78f8e8d552c463a957aa065a180be82223e40fdf
caleron <caleron@users.noreply.github.com>
2017-11-04 17:07:14 +0100

    added links to docs

--- functions.py
+++ functions.py
@@ -4,9 +4,11 @@ import networkx as nx
 def basic_numbers(g: nx.Graph):
     print(f"Number of Nodes (Order):  {g.number_of_nodes()}")
     print(f"Number of Edges (Size): {g.number_of_edges()}")
+    # https://networkx.github.io/documentation/stable/reference/generated/networkx.classes.function.density.html
     print(f"Density {str(nx.density(g))}")
 
 
+# https://networkx.github.io/documentation/stable/reference/algorithms/generated/networkx.algorithms.approximation.clustering_coefficient.average_clustering.html
 def clustering_coefficient(g: nx.Graph):
     if g.is_directed():
         print("clustering coefficient not implemented for directed graphs")
@@ -14,6 +16,15 @@ def clustering_coefficient(g: nx.Graph):
         print(f"clustering coefficient: {str(nx.average_clustering(g))}")
 
 
+# https://networkx.github.io/documentation/stable/reference/algorithms/generated/networkx.algorithms.cluster.clustering.html
+def clustering_distribution(g: nx.Graph):
+    # computes clustering coefficient for each node
+    # result is a map: node -> coefficient
+    clustering: dict = nx.clustering(g)
+    print(f"clustering: {str(clustering)}")
+
+
+# https://networkx.github.io/documentation/stable/reference/algorithms/generated/networkx.algorithms.shortest_paths.generic.average_shortest_path_length.html
 def average_shortest_path_length(g):
     if not g.is_directed() and nx.is_connected(g):
         print(f"Average shortest path length: {str(nx.average_shortest_path_length(g))}")
@@ -21,6 +32,7 @@ def average_shortest_path_length(g):
         print("cant compute average shortest path length, graph directed or graph not connected")
 
 
+# https://networkx.github.io/documentation/stable/reference/algorithms/generated/networkx.algorithms.distance_measures.diameter.html
 def diameter(g: nx.Graph):
     if not g.is_directed() and nx.is_connected(g):
         print(f"Diameter: {nx.diameter(g)}")
@@ -28,5 +40,6 @@ def diameter(g: nx.Graph):
         print("Diameter not computable, graph not connected or is directed")
 
 
+# https://networkx.github.io/documentation/stable/reference/algorithms/generated/networkx.algorithms.approximation.connectivity.node_connectivity.html
 def connectivity(g: nx.Graph):
     print(f"Connectivity: {nx.node_connectivity(g)}")
--- main.py
+++ main.py
@@ -15,13 +15,21 @@ import functions
 # facebook (undirected, connected)
 #   http://snap.stanford.edu/data/facebook.tar.gz
 
+# zero access (directed)
+#   https://github.com/iBigQ/botnet-graphs/blob/master/individual_snapshots/za_16464_core_min.graphml.gz
 
 def analyze_net(g: nx.Graph, name: str):
     print(f"Analyzing {name}")
+    # https://networkx.github.io/documentation/stable/reference/classes/generated/networkx.Graph.to_undirected.html
+    g = nx.to_undirected(g)
+
+    # https://networkx.github.io/documentation/stable/reference/algorithms/generated/networkx.algorithms.components.is_connected.html
+    print(f"Graph is connected: {str(nx.is_connected(g))}")
 
     functions.basic_numbers(g)
     functions.average_shortest_path_length(g)
     functions.clustering_coefficient(g)
+    functions.clustering_distribution(g)
     functions.diameter(g)
     functions.connectivity(g)
 
@@ -50,6 +58,13 @@ def facebook_combined():
     analyze_net(g, name)
 
 
+def zero_access():
+    name = "zero access core min"
+    print(f"loading {name}")
+    g = nx.read_graphml("graphs/GraphZeroAccess_SuperpeersAt2016-02-23_03-19-30_anonym.graphml")
+    analyze_net(g, name)
+
+
 def facebook_0():
     name = "facebook"
     print(f"Loading {name}")
@@ -57,6 +72,8 @@ def facebook_0():
     analyze_net(g, name)
 
 
-facebook_combined()
+# facebook_0()
+# zero_access()
+# facebook_combined()
 # sality()
-# gnutella()
+gnutella()
